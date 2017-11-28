3af435bd959b2b8f500a9212449903a0dc9d6b73
Ross Hill <rosshill88@gmail.com>
2017-11-28 20:37:51 +0000

    name tests passing for all classes

--- specs/bears_spec.rb
+++ specs/bears_spec.rb
@@ -4,4 +4,12 @@ require_relative('../bears.rb')
 
 class BearsTest < MiniTest::Test
 
+  def setup()
+    @name = Bear.new("Winnie")
+  end
+
+  def test_bear_has_name()
+    assert_equal("Winnie", @name.name())
+  end
+
 end
--- specs/fish_spec.rb
+++ specs/fish_spec.rb
@@ -4,4 +4,12 @@ require_relative('../fish.rb')
 
 class FishTest < MiniTest::Test
 
+  def setup()
+    @name = Fish.new("Nemo")
+  end
+
+  def test_fish_has_name()
+    assert_equal("Nemo", @name.name())
+  end
+
 end
